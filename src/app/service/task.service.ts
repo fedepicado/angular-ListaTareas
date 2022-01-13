727f74b58241fee486f190782c0c7406d98bac14
federico picado <fedepicado@gmail.com>
2022-01-13 00:11:32 -0300

    borrar tarea y cambia color

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import {HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Observable, of} from 'rxjs';
 import { Task } from '../Task';
 import { TASKS} from '../monk-tasks'
 
+const httpOptions= {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl='http://localhost:5000/task'
+  private apiUrl= 'http://localhost:5000/task'
 
   constructor(
     private http:HttpClient 
@@ -18,6 +23,15 @@ export class TaskService {
 
   getTask(): Observable <Task[]> {
     return this.http.get<Task[]>(this.apiUrl)
+  }
+  
+  deleteTask(task:Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
 
+  updateTaskReminder(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url,task, httpOptions)
   }
 }
